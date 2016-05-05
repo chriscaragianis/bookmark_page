a00d22f6893a12df73a3319994de612d7e234f7a
Chris Caragianis <chriscaragianis@gmail.com>
2016-05-05 18:55:40 -0400

    Add list structure

--- lib/bookmarker.rb
+++ lib/bookmarker.rb
@@ -9,3 +9,48 @@ def make_head
   out
 end
 
+def make_body input
+  open_tags = tag_hash ["ul", "a", "li"]
+  lines = input.split("\n")
+  out = "  <body>\n"
+  lines.each do |l|
+    if l.include? "<DL>" then
+      if open_tags["ul"] then
+        out << "</ul>\n"
+        out << "<ul>\n"
+      else
+        out << "<ul>\n"
+        open_tags["ul"] = true
+      end
+    end
+    if l.include? "<A HREF" then
+      out << "<li>\n"
+      out << "<a href>\n"
+      out << "</a>\n"
+      out << "</li>\n"
+    end
+  end
+  open_tags.keys.each do |tag|
+    if open_tags[tag] then
+      out << "</#{tag}>\n"
+    end
+  end
+
+  Dir.foreach("src") do |fname|
+    if fname.slice(-3, 3) == ".js" then
+      out << "    <script type=\"text/javascript\" src=\"src/#{fname}\"></script>\n"
+    end
+  end
+  out << "  </body>"
+  out
+end
+
+def tag_hash tag_list
+  out = Hash.new
+  tag_list.each do |a|
+    out[a] = false
+  end
+  out
+end
+
+
